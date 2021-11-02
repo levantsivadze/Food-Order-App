95f84ca85b952ece3035cb90793674706b2f84d2
Levan Tsivadze <levani.tsivadze@gmail.com>
2021-11-02 11:41:14 +0400

    first commit

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from "./AvailableMeals.module.css";
+import DUMMY_MEALS from "./dummy-meals";
+import MealItem from "./MealItem/MealItem";
+
+function AvailableMeals() {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+}
+
+export default AvailableMeals;
